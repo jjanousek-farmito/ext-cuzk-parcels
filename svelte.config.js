bfd017d3f5ed146d7f42baa5a505cc666c026bea
Jáchym Janoušek <janousekjachym@gmail.com>
2025-04-03 06:59:01 +0200

    Refactor project structure: remove Tailwind and PostCSS configurations, add component aliases, and update TypeScript settings

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,15 @@ export default {
     // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
     // for more information about preprocessors
     preprocess: vitePreprocess(),
+    alias: {
+        $components: "src/components",
+        $stores: "src/stores",
+        $utils: "src/utils",
+        $assets: "src/assets",
+        "@/*": "src/lib/*",
+    },
+    external: [
+        "src/lib/components/ui/*"
+    ],
+
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+import path from "path"
 import { crx } from "@crxjs/vite-plugin";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite";
 import manifest from "./src/manifest.config";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [svelte(), crx({ manifest })],
+    plugins: [tailwindcss(), svelte(), crx({ manifest })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src/lib"),
+        },
+    },
+
     // HACK: https://github.com/crxjs/chrome-extension-tools/issues/696
     // https://github.com/crxjs/chrome-extension-tools/issues/746
     server: {
